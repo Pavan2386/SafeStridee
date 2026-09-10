e7061a2177b6e0aa4135b9ea8c18514f899160ba
Pavan2386 <puttapavan25@gmail.com>
2026-09-10 06:25:14 +0530

    Improve navigation functionality

--- nav_engine.py
+++ nav_engine.py
@@ -1,6 +1,7 @@
 import requests
 import re
 
+
 class NavEngine:
     def __init__(self, maps_key):
         self.key = maps_key
@@ -8,13 +9,28 @@ class NavEngine:
 
     def get_step_directions(self, origin_lat_lon, destination):
         params = {
-            'origin': origin_lat_lon, 
-            'destination': destination, 
-            'mode': 'walking', 
+            'origin': origin_lat_lon,
+            'destination': destination,
+            'mode': 'walking',
             'key': self.key
         }
+
         res = requests.get(self.url, params=params).json()
+
         if res['status'] == 'OK':
-            instruction = res['routes'][0]['legs'][0]['steps'][0]['html_instructions']
-            return re.sub('<[^<]+?>', '', instruction) # Strip HTML tags
-        return "Route calculation in progress..."
+            steps = res['routes'][0]['legs'][0]['steps']
+
+            directions = []
+
+            for step in steps:
+                instruction = step['html_instructions']
+                clean_instruction = re.sub(
+                    '<[^<]+?>',
+                    '',
+                    instruction
+                )
+                directions.append(clean_instruction)
+
+            return directions
+
+        return ["Route calculation in progress..."]
